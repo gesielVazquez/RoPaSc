72375dd1cc92fd520c2905b3a9c694394745e875
gesielVazquez <the.alucard.legna@gmail.com>
2021-12-18 00:47:58 -0600

    Segundo paso, captura de la opcion del usuario, con formato ya compatible para comparar

--- Js/ropas.js
+++ Js/ropas.js
@@ -7,3 +7,15 @@ function computerPlay(){
 }
 
 console.log(computerPlay());
+
+function playerPlay(){
+    let ansEnter = (prompt("Please enter your chose Rock, Paper, Scissors: "));
+    let lower = ansEnter.toLowerCase();
+    let first = lower.slice(0, 1);
+    let second = lower.slice(1);
+    let answer = first.toUpperCase() + second;
+    return answer;
+}
+
+console.log(playerPlay());
+
